b1fe93cfbc2c79b661b09483dd8823b85541a12a
GRACENOBLE <gracenoble72@gmail.com>
2025-02-08 08:59:16 +0300

    refactor(server): remove unused database connection logic and clean up imports

--- app/server.go
+++ app/server.go
@@ -1,42 +1,18 @@
 package app
 
 import (
-	"context"
-	"fmt"
 	"log"
 	"os"
 
 	"github.com/GRACENOBLE/ecommerce/api"
-	"github.com/GRACENOBLE/ecommerce/database"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 )
-
-//the function below
 
 func Router() {
 	err := godotenv.Load()
 	if err != nil {
 		log.Println("No .env file found, using defaults.")
-	}
-
-	db := database.ConnectDatabase()
-	defer db.Close()
-
-	rows, err := db.Query(context.Background(), "SELECT id, name FROM users")
-	if err != nil {
-		log.Fatalf("Failed to query data: %v\n", err)
-	}
-	defer rows.Close()
-
-	for rows.Next() {
-		var id string
-		var name string
-		err := rows.Scan(&id, &name)
-		if err != nil {
-			log.Fatalf("Failed to scan row: %v\n", err)
-		}
-		fmt.Printf("ID: %s, Name: %s\n", id, name)
 	}
 
 	r := gin.Default()
