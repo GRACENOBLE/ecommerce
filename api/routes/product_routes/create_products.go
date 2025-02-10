0170e74e1b23049f75a3645de6c870520e7e7094
GRACENOBLE <gracenoble72@gmail.com>
2025-02-10 06:42:38 +0300

    feat(routes): implement CreateProduct handler to add new products and update Product type

--- api/routes/product_routes/create_products.go
+++ api/routes/product_routes/create_products.go
@@ -1,7 +1,34 @@
 package productroutes
 
-import "github.com/gin-gonic/gin"
+import (
+	"context"
+	"fmt"
+
+	"github.com/GRACENOBLE/ecommerce/database"
+	"github.com/GRACENOBLE/ecommerce/internal/types"
+	"github.com/gin-gonic/gin"
+)
 
 func CreateProduct(c *gin.Context) {
-	// Handler logic for creating a product
+	var newProduct types.Product
+
+	if err := c.ShouldBindJSON(&newProduct); err != nil {
+		c.JSON(400, gin.H{"error": err.Error()})
+		return
+	}
+
+	fmt.Printf("%v",newProduct)
+
+	db := database.ConnectDatabase()
+	defer db.Close()
+
+	query := `INSERT INTO products (name, description, price, stock_quantity, image_url, created_at, updated_at) 
+			  VALUES ($1, $2, $3, $4, $5, NOW(), NOW()) RETURNING id`
+	err := db.QueryRow(context.Background(), query, newProduct.Name, newProduct.Description, newProduct.Price, newProduct.StockQuantity, newProduct.ImageURL).Scan(&newProduct.Id)
+	if err != nil {
+		c.JSON(500, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(201, newProduct)
 }
--- internal/types/product.go
+++ internal/types/product.go
@@ -6,8 +6,8 @@ type Product struct {
 	Id            string    `json:"id"`
 	Name          string    `json:"name"`
 	Description   string    `json:"description"`
-	Price         int64     `json:"price"`
-	StockQuantity string    `json:"stock"`
+	Price         float64     `json:"price"`
+	StockQuantity int64    `json:"stock"`
 	ImageURL      string    `json:"image"`
 	CreatedAt     time.Time `json:"created_at"`
 	UpdatedAt     time.Time `json:"updated_at"`
