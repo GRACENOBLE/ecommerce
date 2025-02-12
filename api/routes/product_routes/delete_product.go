a34b8a930c5c743683076b466f2685ebfac9fe68
GRACENOBLE <gracenoble72@gmail.com>
2025-02-12 07:13:52 +0300

    feat(routes): implement DeleteProduct handler to remove products by ID

--- api/routes/product_routes/delete_product.go
+++ api/routes/product_routes/delete_product.go
@@ -1,7 +1,40 @@
 package productroutes
 
-import "github.com/gin-gonic/gin"
+import (
+	"context"
+
+	"github.com/GRACENOBLE/ecommerce/database"
+	"github.com/gin-gonic/gin"
+)
 
 func DeleteProduct(c *gin.Context) {
-	// Handler logic for deleting a product
+	id := c.Param("id")
+	if id == "" {
+		c.JSON(400, gin.H{"error": "Missing product ID"})
+		return
+	}
+
+	db := database.ConnectDatabase()
+	defer db.Close()
+
+	query := `DELETE FROM products WHERE id = $1;`
+
+	result, err := db.Exec(context.Background(), query, id)
+	if err != nil {
+		c.JSON(500, gin.H{"error": err.Error()})
+		return
+	}
+
+	rowsAffected := result.RowsAffected()
+
+	if rowsAffected == 0 {
+		c.JSON(404, gin.H{"error": "Product not found"})
+		return
+	}
+
+	c.JSON(200, gin.H{
+		"rows affected": rowsAffected,
+		"deleted":       id,
+	})
+
 }
