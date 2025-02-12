ec631be5ac2248ec1ded14531d74f6bb052558ad
GRACENOBLE <gracenoble72@gmail.com>
2025-02-12 06:32:17 +0300

    feat(routes): refactor CreateProduct and implement UpdateProduct handler for product management

--- api/routes/product_routes/create_products.go
+++ api/routes/product_routes/create_products.go
@@ -2,7 +2,6 @@ package productroutes
 
 import (
 	"context"
-	"fmt"
 
 	"github.com/GRACENOBLE/ecommerce/database"
 	"github.com/GRACENOBLE/ecommerce/internal/types"
@@ -17,13 +16,11 @@ func CreateProduct(c *gin.Context) {
 		return
 	}
 
-	fmt.Printf("%v",newProduct)
-
 	db := database.ConnectDatabase()
 	defer db.Close()
 
-	query := `INSERT INTO products (name, description, price, stock_quantity, image_url, created_at, updated_at) 
-			  VALUES ($1, $2, $3, $4, $5, NOW(), NOW()) RETURNING id`
+	query := `INSERT INTO products (name, description, price, stock_quantity, image_url, created_at, updated_at) VALUES ($1, $2, $3, $4, $5, NOW(), NOW()) RETURNING id`
+	
 	err := db.QueryRow(context.Background(), query, newProduct.Name, newProduct.Description, newProduct.Price, newProduct.StockQuantity, newProduct.ImageURL).Scan(&newProduct.Id)
 	if err != nil {
 		c.JSON(500, gin.H{"error": err.Error()})
