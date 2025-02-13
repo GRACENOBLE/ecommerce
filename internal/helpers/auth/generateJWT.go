a26be3000a57b8a237890e36c48fa8e5231ce995
GRACENOBLE <gracenoble72@gmail.com>
2025-02-13 08:30:11 +0300

    feat(auth): refactor JWT generation and validation functions for improved clarity and functionality

--- internal/helpers/auth/generateJWT.go
+++ internal/helpers/auth/generateJWT.go
@@ -9,18 +9,28 @@ import (
 	"github.com/joho/godotenv"
 )
 
-// GenerateJWT generates a JWT token for a user
-func GenerateJWT(userID string) (string, error) {
+// CreateToken generates a JWT for a given user email and role.
+func CreateToken(email, role string) (string, error) {
 
 	err := godotenv.Load()
 	if err != nil {
 		log.Println("No .env file found, using defaults.")
 	}
-	jwtSecret := []byte(os.Getenv("JWT_SECRET"))
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
-		"user_id": userID,
-		"exp":     time.Now().Add(time.Hour * 24).Unix(), // Token valid for 24 hours
-	})
-	return token.SignedString(jwtSecret)
 
+	jwtSecretKey := []byte(os.Getenv("JWT_SECRET"))
+	token := jwt.New(jwt.SigningMethodHS256)
+
+	// Set claims
+	claims := token.Claims.(jwt.MapClaims)
+	claims["authorized"] = true
+	claims["email"] = email
+	claims["role"] = role
+	claims["exp"] = time.Now().Add(15 * time.Minute).Unix()
+
+	// Sign and get the complete encoded token as a string
+	tokenString, err := token.SignedString(jwtSecretKey)
+	if err != nil {
+		return "", err
+	}
+	return tokenString, nil
 }
--- internal/helpers/auth/parse&validateJWT.go
+++ internal/helpers/auth/parse&validateJWT.go
@@ -2,25 +2,25 @@ package auth
 
 import (
 	"fmt"
+	"log"
 	"os"
 
 	"github.com/golang-jwt/jwt/v5"
+	"github.com/joho/godotenv"
 )
 
-func ParseJWT(tokenString string) (string, error) {
-
-	jwtSecret := []byte(os.Getenv("JWT_SECRET"))
-
-	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
-		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
-		}
-		return jwtSecret, nil
-	})
-
-	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
-		userID := claims["user_id"].(string)
-		return userID, nil
+func VerifyToken(tokenString string) (*jwt.Token, error) {
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("No .env file found, using defaults.")
 	}
-	return "", err
+	jwtSecretKey := []byte(os.Getenv("JWT_SECRET"))
+	token, err := jwt.Parse(tokenString, func(t *jwt.Token) (interface{}, error) {
+		// Ensure token's signing method is HS256
+		if _, ok := t.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", t.Header["alg"])
+		}
+		return jwtSecretKey, nil
+	})
+	return token, err
 }
