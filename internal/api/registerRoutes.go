87a26c96a0774a0e2a0d4aa2689c29f28159c485
GRACENOBLE <gracenoble72@gmail.com>
2025-03-05 06:46:13 +0300

    feat(cors): add CORS middleware to allow specific origins and methods

--- internal/api/registerRoutes.go
+++ internal/api/registerRoutes.go
@@ -1,11 +1,21 @@
 package api
 
 import (
+	"time"
+
 	"github.com/GRACENOBLE/ecommerce/internal/helpers/middleware"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func RegisterRoutes(r *gin.Engine, dbConfig *DBConfig) {
+	r.Use(cors.New(cors.Config{
+        AllowOrigins:     []string{"http://localhost:3000"},
+        AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+        AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization"},
+        AllowCredentials: true,
+        MaxAge:           12 * time.Hour,
+    }))
 
 	productRoutes := r.Group("/products")
 	productRoutes.Use(middleware.JWTAuthMiddleware())
